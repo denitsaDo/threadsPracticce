fd51831a9fa6ef47a6847115dcc35b5edd6e09e3
Denitsa Dochkova <denitsa.dochkova@gmail.com>
2022-01-08 22:02:43 +0200

    Added Lesson 1 and Lesson 2

---
+++ src/Video1/lesson1/Demo.java
@@ -0,0 +1,25 @@
+package Video1.lesson1;
+
+public class Demo {
+    public static void main(String[] args) {
+        MyClass myClass = new MyClass();
+        myClass.start();
+
+        MyClass myClass1 = new MyClass();
+        myClass1.start();
+        MyClass myClass2 = new MyClass();
+        myClass2.start();
+        MyClass myClass3 = new MyClass();
+        myClass3.start();
+        MyClass myClass4 = new MyClass();
+        myClass4.start();
+        MyClass myClass5 = new MyClass();
+        myClass5.start();
+        MyClass myClass6 = new MyClass();
+        myClass6.start();
+        MyClass myClass7 = new MyClass();
+        myClass7.start();
+
+
+    }
+}
